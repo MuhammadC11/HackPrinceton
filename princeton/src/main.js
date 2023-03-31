0b8c9bc7c29f92582408c6df108f82d39ce8cef5
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2023-03-31 17:31:22 -0400

    added base template

--- princeton/src/main.js
+++ princeton/src/main.js
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import "./style.css";
+import App from "./App.vue";
+import Home from "./views/Home.vue";
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", name: "home", component: Home },
+
+    // { path: "/contact", name: "contact", component: Contact },
+  ],
+});
+
+createApp(App).use(router).mount("#app");
