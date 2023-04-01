a0a7cdebe09dc9415c3205ab5df186c84aa0707f
Matthew LaBarca <matlab0510@gmail.com>
2023-04-01 09:50:23 -0400

    update login text in App

--- princeton/src/App.vue
+++ princeton/src/App.vue
@@ -4,7 +4,7 @@
 <template>
   <nav> 
     <router-link to="/">Home</router-link> |
-    <router-link to="/login">Login</router-link>
+    <router-link to="/login">Login</router-link> |
     <router-link to="/register">Register</router-link>
   </nav>
   <router-view />
