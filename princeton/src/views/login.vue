ed5a1e9fd9792786910deb55299d819367fcd138
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2023-04-01 01:58:21 -0400

    updated login css

--- princeton/src/views/login.vue
+++ princeton/src/views/login.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="login_container">
-    <form>
+  <div>
+    <form class="login_container">
       <div class="inputs">
         <label id="el"> Email: </label>
         <input
@@ -35,109 +35,77 @@
   margin-top: 5rem;
 }
 
-#email {
-  text-align: center;
-  width: 20rem;
-  height: 4rem;
-  border: none;
-  font-size: 1.5rem;
-  font-weight: 500;
-  color: #ffffff;
-  background-color: #0000006a;
-  outline: none;
-  transition: all 0.2s ease-in-out;
-  &:focus {
-    background-color: rgba(0, 0, 0, 0.602);
-  }
-}
-#password {
-  text-align: center;
-  width: 20rem;
-  height: 4rem;
-  border: none;
-  font-size: 1.5rem;
-  font-weight: 500;
-  color: #ffffff;
-  background-color: #0000006a;
-  outline: none;
-  transition: all 0.2s ease-in-out;
-  &:focus {
-    background-color: rgba(0, 0, 0, 0.602);
-  }
-}
-
 .inputs {
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   width: 100%;
+  margin-top: 5rem;
 }
 
-.search-btn {
+#el {
   font-size: 1.5rem;
-  border-radius: 1rem;
-  margin-left: 0.5rem;
-  padding: 1rem;
-  display: inline-block;
-  cursor: pointer;
   color: #ffffff;
-  padding: 1.2rem;
-  font-weight: 700;
-  transition: transform 200ms, background 200ms;
+  font-family: "Quicksand", sans-serif;
+  margin: auto;
 }
-.submit-btn {
-  // create a hover effect that makes the button a little bit bigger and darker
-  background-color: rgba(39, 38, 38, 0.979);
-  transition: all 0.3s ease-in-out;
-  cursor: pointer;
-}
-.submit-btn:hover {
-  // create a hover effect that makes the button a little bit bigger and darker
-  transform: scale(1.1);
-  transition: all 0.3s ease-in-out;
-}
-.clear-btn {
+
+#pl {
   font-size: 1.5rem;
-  border-radius: 1rem;
-  margin-left: 0.5rem;
-  padding: 1rem;
-  display: inline-block;
-  cursor: pointer;
   color: #ffffff;
-  padding: 1.2rem;
-  font-weight: 700;
-  transition: transform 200ms, background 200ms;
-}
-.clear-btn:hover {
-  // create a hover effect that makes the button a little bit bigger and darker
-
-  transform: translateY(-2px);
-}
-.clear-btn:focus-visible {
-  outline: 1px solid #000;
-  outline-offset: 1px;
+  font-family: "Quicksand", sans-serif;
+  margin: auto;
 }
 
-.item {
-  display: flex;
-  align-items: center;
-  height: 5rem;
-  width: 100vw;
-  background-color: transparent;
-
-  border-radius: 0.5rem;
-  cursor: pointer;
+input {
+  text-align: center;
+  width: 20rem;
+  height: 4rem;
+  border: none;
+  font-size: 1.5rem;
+  font-weight: 500;
+  color: #ffffff;
+  background-color: #0000006a;
+  outline: none;
   transition: all 0.2s ease-in-out;
-  &:hover {
-    background-color: rgba(0, 0, 0, 0.478);
+  &:focus {
+    background-color: rgba(0, 0, 0, 0.602);
   }
-  p {
-    color: #ffffff;
-    font-size: 1.5rem;
-    font-weight: 500;
-    text-align: center;
-    width: 100%;
+}
+
+.submit-btn {
+  text-align: center;
+  width: 10rem;
+  height: 3rem;
+  border: none;
+  font-size: 1.5rem;
+  font-weight: 500;
+  color: #ffffff;
+  background-color: #0000006a;
+  outline: none;
+  transition: all 0.2s ease-in-out;
+  &:focus {
+    background-color: rgba(0, 0, 0, 0.602);
   }
+  margin-top: 1rem;
+}
+
+body {
+  background-color: #0094fe;
+}
+.title {
+  text-align: center;
+  font-size: 3rem;
+  color: #ffffff;
+  font-family: "Quicksand", sans-serif;
+}
+.survey_input {
+  font-size: 1.5rem;
+  color: #ffffff;
+  font-family: "Quicksand", sans-serif;
+
+  margin: auto;
 }
 </style>
 
