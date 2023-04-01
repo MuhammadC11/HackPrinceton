f77eb82ef8adb38d9559b52789a57ed3dfb126c9
nenebot <nene.bot@icloud.com>
2023-04-01 13:42:12 -0400

    something

---
+++ princeton/src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1> Signed in sucessfully </h1>
+</template>
+<script>
+
+</script>
+<style>
+    h1 {
+        text-align: center;
+    }
+    p {
+        text-align: center;
+    }
+    button {
+        text-align: center;
+    }
+</style>
