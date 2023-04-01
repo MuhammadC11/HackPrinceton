bdfc51d992f4d6dc24e26fe2530715e35a0491a8
nenebot <nene.bot@icloud.com>
2023-04-01 09:37:29 -0400

    auth-working

--- princeton/src/router/index.js
+++ princeton/src/router/index.js
@@ -4,7 +4,7 @@ const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: "/", name: "home", component: () => import("../views/Home.vue") },
-        { path: "/login", name: "login", component: () => import("../views/Login.vue") },
+        { path: "/login", name: "login", component: () => import("../views/login.vue") },
         { path: "/register", name: "register", component: () => import("../views/Register.vue") },
     ],});
 export default router;
--- princeton/src/views/Register.vue
+++ princeton/src/views/Register.vue
@@ -2,7 +2,7 @@
     <h1>Create an account</h1>
     <p><input type="text" placeholder="Email Address" v-model="email"/></p>
     <p><input type="password" placeholder="Password" v-model="password"/></p>
-    <p><button @click="register">Submit</button></p>
+    <p><button @click="register">Sign Up</button></p>
 </template>
 
 <script setup>
